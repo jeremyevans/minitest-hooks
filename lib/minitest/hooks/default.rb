227dc764186881494f7565cc24e3b0db54a19159
Janko Marohnić <janko.marohnic@gmail.com>
2015-07-01 10:00:32 +0200

    Don't load minitest/spec in main file
    Avoid loading minitest/spec when requiring `minitest/hooks`, because
    minitest/test users probably don't want minitest/spec to be required,
    since it does a lot of moneky-patching.
    
    The functionality should stay the same for the majority of use cases,
    because requiring `minitest/hooks` itself still doesn't add
    `Minitest::Hooks` it to `Minitest::Spec`, only `minitest/hooks/default`
    does.

--- lib/minitest/hooks/default.rb
+++ lib/minitest/hooks/default.rb
@@ -1,3 +1,9 @@
 require 'minitest/hooks'
+require 'minitest/spec'
+
+# Spec subclass that includes the hook methods.
+class Minitest::HooksSpec < Minitest::Spec
+  include Minitest::Hooks
+end
 
 MiniTest::Spec.register_spec_type(//, Minitest::HooksSpec)
