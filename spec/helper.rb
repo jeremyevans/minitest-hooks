ce134f2dd286c138a16023dc0dd602f8445e19fd
Jeremy Evans <code@jeremyevans.net>
2019-05-31 10:12:57 -0700

    Do not load minitest plugins when testing

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,5 +2,6 @@ $:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), "../lib/"))
 require 'rubygems'
 require 'sequel'
 gem 'minitest'
+ENV['MT_NO_PLUGINS'] = '1' # Work around stupid autoloading of plugins
 require 'minitest/autorun'
 DATABASE_URL = ENV['DATABASE_URL'] || (defined?(JRUBY_VERSION) ? 'jdbc:sqlite::memory:' : 'sqlite:/')
