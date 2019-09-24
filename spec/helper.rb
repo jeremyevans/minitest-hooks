94bec27e6426f1acc45c7cc950f8b922a5e5521a
Jeremy Evans <code@jeremyevans.net>
2019-09-24 08:24:47 -0700

    Use minitest-global_expecations in tests to avoid deprecation issues with minitest 5.12
    Also, force minitest 5.11.3 on ruby <2.4, as minitest 5.12.0
    breaks on ruby <2.4.
    
    Also, disable automatic loading of minitest plugins when running tests.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), "../lib/"))
 require 'rubygems'
 require 'sequel'
-gem 'minitest'
 ENV['MT_NO_PLUGINS'] = '1' # Work around stupid autoloading of plugins
-require 'minitest/autorun'
+gem 'minitest'
+require 'minitest/global_expectations/autorun'
 DATABASE_URL = ENV['DATABASE_URL'] || (defined?(JRUBY_VERSION) ? 'jdbc:sqlite::memory:' : 'sqlite:/')
